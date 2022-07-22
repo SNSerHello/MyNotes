e07f40715ac2b4cd6b57c207f5dd70e8cab231fd
SNSerHello <hello.sns@hotmail.com>
2022-07-22 23:12:27 +0800

    [07/22/2022] add switchers for gcc-7/8/9
    add switchers for gcc-7/8/9

---
+++ gcc/switch_gcc7.sh
@@ -0,0 +1,19 @@
+sudo update-alternatives --set gcc /usr/bin/gcc-7
+sudo update-alternatives --set g++ /usr/bin/g++-7
+sudo update-alternatives --set cpp /usr/bin/cpp-7
+sudo update-alternatives --set gcc-ar /usr/bin/gcc-ar-7
+sudo update-alternatives --set gcc-nm /usr/bin/gcc-nm-7
+sudo update-alternatives --set gcc-ranlib /usr/bin/gcc-ranlib-7
+sudo update-alternatives --set gcov /usr/bin/gcov-7
+sudo update-alternatives --set gcov-dump /usr/bin/gcov-dump-7
+sudo update-alternatives --set gcov-tool /usr/bin/gcov-tool-7
+sudo update-alternatives --set x86_64-linux-gnu-gfortran /usr/bin/x86_64-linux-gnu-gfortran-7
+sudo update-alternatives --set x86_64-linux-gnu-gcov /usr/bin/x86_64-linux-gnu-gcov-7
+sudo update-alternatives --set x86_64-linux-gnu-gcov-tool /usr/bin/x86_64-linux-gnu-gcov-tool-7
+sudo update-alternatives --set x86_64-linux-gnu-gcov-dump /usr/bin/x86_64-linux-gnu-gcov-dump-7
+sudo update-alternatives --set x86_64-linux-gnu-gcc-ranlib /usr/bin/x86_64-linux-gnu-gcc-ranlib-7
+sudo update-alternatives --set x86_64-linux-gnu-gcc-ar /usr/bin/x86_64-linux-gnu-gcc-ar-7
+sudo update-alternatives --set x86_64-linux-gnu-gcc /usr/bin/x86_64-linux-gnu-gcc-7
+sudo update-alternatives --set x86_64-linux-gnu-g++ /usr/bin/x86_64-linux-gnu-g++-7
+sudo update-alternatives --set x86_64-linux-gnu-cpp /usr/bin/x86_64-linux-gnu-cpp-7
+
---
+++ gcc/switch_gcc8.sh
@@ -0,0 +1,19 @@
+sudo update-alternatives --set gcc /usr/bin/gcc-8
+sudo update-alternatives --set g++ /usr/bin/g++-8
+sudo update-alternatives --set cpp /usr/bin/cpp-8
+sudo update-alternatives --set gcc-ar /usr/bin/gcc-ar-8
+sudo update-alternatives --set gcc-nm /usr/bin/gcc-nm-8
+sudo update-alternatives --set gcc-ranlib /usr/bin/gcc-ranlib-8
+sudo update-alternatives --set gcov /usr/bin/gcov-8
+sudo update-alternatives --set gcov-dump /usr/bin/gcov-dump-8
+sudo update-alternatives --set gcov-tool /usr/bin/gcov-tool-8
+sudo update-alternatives --set x86_64-linux-gnu-gfortran /usr/bin/x86_64-linux-gnu-gfortran-8
+sudo update-alternatives --set x86_64-linux-gnu-gcov /usr/bin/x86_64-linux-gnu-gcov-8
+sudo update-alternatives --set x86_64-linux-gnu-gcov-tool /usr/bin/x86_64-linux-gnu-gcov-tool-8
+sudo update-alternatives --set x86_64-linux-gnu-gcov-dump /usr/bin/x86_64-linux-gnu-gcov-dump-8
+sudo update-alternatives --set x86_64-linux-gnu-gcc-ranlib /usr/bin/x86_64-linux-gnu-gcc-ranlib-8
+sudo update-alternatives --set x86_64-linux-gnu-gcc-ar /usr/bin/x86_64-linux-gnu-gcc-ar-8
+sudo update-alternatives --set x86_64-linux-gnu-gcc /usr/bin/x86_64-linux-gnu-gcc-8
+sudo update-alternatives --set x86_64-linux-gnu-g++ /usr/bin/x86_64-linux-gnu-g++-8
+sudo update-alternatives --set x86_64-linux-gnu-cpp /usr/bin/x86_64-linux-gnu-cpp-8
+
